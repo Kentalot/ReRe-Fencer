52db1025c9704a99ea24cad1132b0654425d2133
Kentalot <kho@illumina.com>
2015-01-10 08:15:16 -0800

    Initial Setup

---
+++ rere-fencer/rere-fencer/Processors/RRFProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rere_fencer.Processors
+{
+    public class RRFProcessor : IRRFProcessor
+    {
+
+    }
+}
---
+++ rere-fencer/rere-fencer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDesk.Options;
+using rere_fencer.Input;
+using rere_fencer.Processors;
+
+namespace rere_fencer
+{
+    class Program
+    {
+        private static readonly OptionSet _optionSet = new OptionSet();
+        public static FileInfo GenomeFilePath { get; private set; }
+
+        static void Main(string[] args)
+        {
+            InitializeOptionSet();
+            new Rere_fencerLauncher(new GenomeReader(), new VCFReader(), new RRFProcessor(), new RRFResolver(), new GenomeWriter(), args).Launch();
+        }
+
+        private static void InitializeOptionSet()
+        {
+            _optionSet.Add("g|genomePath=", "Path to the Genome input file. Must be in one fasta file",
+                s => GenomeFilePath = UpdateFileInfo(s));
+        }
+
+        private static FileInfo UpdateFileInfo(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath)) throw new ArgumentException("Invalid Blank filePath input!");
+            var file = new FileInfo(filePath);
+            var invalid = !file.Exists || file.Length < 100;
+            if (invalid) throw new ArgumentException("File not found or file too small to be input!");
+            return file;
+        }
+    }
+}
