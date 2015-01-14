595463d9ed37d2d7095ad286fe9b35136f31e35a
Kentalot <kho@illumina.com>
2015-01-13 17:03:18 -0800

    Added fa2bit.jar but not the rest of the infrastructure.  Added more details to all the TwoBitGenomeReaders.

---
+++ rere-fencer/rere-fencer/Exceptions/InvalidTwoBitFileException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rere_fencer.Exceptions
+{
+    public class InvalidTwoBitFileException : Exception
+    {
+        public InvalidTwoBitFileException(string file, string message) 
+            : base(file + " file is not a valid TwoBit file.\n" + message) { }
+    }
+}
